a15a6535ddff0fb4f27f6a72602a5e25fba8b30a
MihaelGolob <golob.mihael@gmail.com>
2022-09-07 12:25:05 +0200

    after testing I figured that startOnAwake parameter should be true by default

--- Runtime/Timer.cs
+++ Runtime/Timer.cs
@@ -21,10 +21,10 @@ public class Timer {
     private bool _dirty;
 
     // constructor (set isRunning to true automatically if startOnAwake is true)
-    public Timer(float time, Action callback, bool loop = false, bool startOnAwake = false) =>
+    public Timer(float time, Action callback, bool loop = false, bool startOnAwake = true) =>
         (_time, _loop, _callback, _isRunning) = (time, loop, callback, startOnAwake);
     
-    public Timer(float time, bool loop = false, bool startOnAwake = false) =>
+    public Timer(float time, bool loop = false, bool startOnAwake = true) =>
         (_time, _loop, _isRunning) = (time, loop, startOnAwake);
 
     // public fields
